ae7182f0945744151a6c4e7de182fa2afea1a9b2
Alter_LIiu <91472748+Alter-Liu@users.noreply.github.com>
2023-08-09 17:01:21 +0800

    Update glados.py

--- glados.py
+++ glados.py
@@ -4,7 +4,7 @@ import requests,json,os
 # -------------------------------------------------------------------------------------------
 if __name__ == '__main__':
 # pushplus秘钥 申请地址 http://www.pushplus.plus
-    sckey = os.environ.get("PUSHPLUS_TOKEN", "")
+    sckeys = os.environ.get("PUSHPLUS_TOKEN", []).split("&")
 # 推送内容
     sendContent = ''
 # glados账号cookie 直接使用数组 如果使用环境变量需要字符串分割一下
@@ -33,10 +33,13 @@ if __name__ == '__main__':
             print(email+'----结果--'+mess+'----剩余('+time+')天')  # 日志输出
             sendContent += email+'----'+mess+'----剩余('+time+')天\n'
         else:
-            requests.get('http://www.pushplus.plus/send?token=' + sckey + '&content='+email+'cookie已失效')
-            print('cookie已失效')  # 日志输出
+            if sckeys != "":
+                for sckey in sckeys:
+                    requests.get('http://www.pushplus.plus/send?token=' + sckey + '&content='+email+'cookie已失效')
+                    print('cookie已失效')  # 日志输出
      #--------------------------------------------------------------------------------------------------------#   
-    if sckey != "":
-         requests.get('http://www.pushplus.plus/send?token=' + sckey + '&title='+email+'签到成功'+'&content='+sendContent)
+    if sckeys != "":
+        for sckey in sckeys:
+            requests.get('http://www.pushplus.plus/send?token=' + sckey + '&title='+email+'签到成功'+'&content='+sendContent)
 
 
